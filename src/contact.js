4017eee08ac338507ad8585c903099e426e8b635
Jayanth9710 <kumarjayanth55@gmail.com>
2021-10-06 18:49:54 +0530

    api

--- src/contact.js
+++ src/contact.js
@@ -12,7 +12,7 @@ function Contact() {
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
   });
  
-const stamenTonerTiles = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=<9091209f-a631-41b6-b5ab-76746082c2c6>';
+const stamenTonerTiles = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
   const stamenTonerAttr = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 //   const position = [51.505, -0.09];
   return (
