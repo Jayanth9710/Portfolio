04935686e4ca10873fcc8d954af05882b5e24040
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2023-09-03 20:25:32 +0530

    Update about.js
    correction of typos

--- src/about.js
+++ src/about.js
@@ -21,14 +21,14 @@ function About() {
             <br /> Nice to meet you!
           </h2>
           <h4 className="desc">
-            I graduated as an Civil engineer, which makes me have profound
-            interests in wide range of disciplines <br />
-            other than the field I graduated.Apart from engineering knowledge, I have substantial<br/> passion for
-            Geography,GIS.In Recent days as learning anything has become easily accessible.I decided to learn Web development.Initially I thought it would be best and easy to learn Frontend development alone, but, some kind and great mentors proved it wrong and I got the opportunity to learn Fullstack with Mongo,Express , ReactJS and NodeJS.
+            I graduated as a Civil engineer, which gave me have profound
+            interests in a wide range of disciplines <br />
+            other than the field I graduated. Apart from engineering knowledge, I have a substantial<br/> passion for
+            Geography, GIS. In recent days learning anything has become easily accessible. I decided to learn Web development. Initially, I thought it would be best and easy to learn Frontend development alone, but, some kind and great mentors proved it wrong and I got the opportunity to learn Fullstack with Mongo, Express, ReactJS, and NodeJS.
           </h4>
           
           <div className="interests">
-            My Multi-Displinary interests and Experiences are :
+            My Multi-Disciplinary interests and Experiences are :
             <ul className="list">
               <li>GIS [Geographical Information System]</li>
               <li>MERN stack Development</li>
@@ -37,7 +37,7 @@ function About() {
             </ul>
           </div>
           <h4 className="desc">
-            I believe in Retraining which makes me to accquire knowledeg about
+            I believe in Retraining which makes me acquire knowledge about
             many topics.
           </h4>
           <div className="interests">
@@ -46,10 +46,10 @@ function About() {
             <ul className="list">
               <li>Listening music</li>
               <li>Football</li>
-              <li>Reading about new techonoligies/gadgets</li>
+              <li>Reading about new technologies/gadgets</li>
             </ul>
           </div>
-          To know more about my Education,skills,experiences and much more go through my Resume below.<br/>
+          To know more about my Education, skills, experiences and much more go through my Resume below.<br/>
           <a href="https://drive.google.com/file/d/1KxjGMPpWOsvn46L3eliYcHK0Ckr087lT/view?usp=sharing" target="_blank" rel="noreferrer" className="resume">Resume</a>
         </div>
       </div>
